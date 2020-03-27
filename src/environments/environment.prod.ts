fcfa4a104b9cd801c5fd8eaea6b56ca10400fc66
Eddie Taliaferro <eddielacrosse2@gmail.com>
2020-03-27 16:08:24 -0400

    Added: PWA files. Still need to update files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+import 'zone.js/dist/zone-error';
+
+const ip = '10.0.1.2';
+const baseUrl = `http://${ip}:3000`;
+
+const url = baseUrl + '/api';    //local host
+
+
 export const environment = {
-  production: true
+  production: true,
+  url: baseUrl
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@
 
 import 'zone.js/dist/zone-error';
 
-const ip = '172.19.44.179';
+const ip = '10.0.1.2';
 const baseUrl = `http://${ip}:3000`;
 
 const url = baseUrl + '/api';    //local host
