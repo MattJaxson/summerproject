e1f55636ef02638a3480a5f966d07beabc852be5
Eddie Taliaferro <eddielacrosse2@gmail.com>
2020-10-30 17:24:35 -0400

    Resources Page refactor

--- src/app/pages/resources/resources.page.ts
+++ src/app/pages/resources/resources.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-resources',
@@ -10,33 +12,39 @@ export class ResourcesPage implements OnInit {
   resources = [
     {
       url: 'https://detroitydrc.z2systems.com/np/clients/detroitydrc/publicaccess/membershipDirectory.do?md=1',
-      logo: '../../assets/ydrc.png',
+      logo: '../../assets/logos/YDRC-logo.png',
       name: 'Detroit DRC',
-      description: 'description'
+      description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim just!'
     },
     {
       url: 'https://discoveryourspark.org/program-finder/#',
-      logo: '../../assets/dys.png',
-      name: 'discoveryourspark',
-      description: 'description'
+      logo: '../../assets/logos/DYS-logo.png',
+      name: 'Discover Your Spark',
+      description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim just!'
     },
     {
       url: 'https://gdyt.org/',
-      logo: '../../assets/gdyt.png',
-      name: 'gdyt',
-      description: 'description'
+      logo: '../../assets/logos/GDYT-logo.png',
+      name: "Grow Detroit's Youth Talent (GDYT)",
+      description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim just!'
     },
     {
       url: 'https://detroitsummerguide.org/',
-      logo: '../../assets/dsg.png',
-      name: 'detroitsummerguide',
-      description: 'description'
+      logo: '../../assets/logos/DSG-logo.png',
+      name: 'Detroit Summer Guide',
+      description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim just!'
     }
   ];
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  back() {
+    this.router.navigate(['']);
   }
 
 }
