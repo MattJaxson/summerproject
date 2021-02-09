3962d2e923aeb576de63424a3b7f06751726c533
Eddie Taliaferro <eddielacrosse2@gmail.com>
2021-02-08 19:25:59 -0500

    updated ip addresses

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,10 @@
 // Production Environment App
 
-const ip = '10.0.1.4';
+const ip = '3.135.225.140';
+const fairIp = '18.191.35.201';
+
 const baseUrl = `http://${ip}:3000`;
-const fairsUrl = `http://${ip}:4000`;
+const fairsUrl = `http://${fairIp}:4000`;
 
 export const environment = {
   production: true,
