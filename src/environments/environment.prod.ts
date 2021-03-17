cedca7b721fe09ac16bef049a3bedd046c8555de
Eddie Taliaferro <eddielacrosse2@gmail.com>
2021-03-16 22:22:56 -0400

    adding port 3000

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@
 const fyfApi = 'findyourfuturesem.org';
 const fairsApi = 'fairs.findyourfuturesem.org';
 
-const baseUrl = `https://${fyfApi}`;
-const fairsUrl = `https://${fairsApi}`;
+const baseUrl = `https://${fyfApi}:3000`;
+const fairsUrl = `https://${fairsApi}:3000`;
 
 export const environment = {
   production: true,
